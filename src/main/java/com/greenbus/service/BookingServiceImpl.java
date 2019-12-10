1d99a3ab7034aff059074869d2c38036f73c1e9d
Akuthota Raghu <akuthota.raghu@hcl.com>
2019-12-10 18:03:45 +0530

    BookingServiceImpl implements BookingService interfece

--- src/main/java/com/greenbus/service/BookingServiceImpl.java
+++ src/main/java/com/greenbus/service/BookingServiceImpl.java
@@ -8,7 +8,7 @@ import com.greenbus.repository.BookingRepository;
  * @author akuthota.raghu
  *
  */
-public class BookingServiceImpl {
+public class BookingServiceImpl implements BookingService{
 	
 	@Autowired
 	private BookingRepository bookingRepository;
